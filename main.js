94bd1dca53cb7736e10b1574f118f8bf3b196660
Lauren Rouse <lauren.l.rouse@gmail.com>
2015-11-09 22:04:39 -0600

    angular 101 described

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 var app = angular.module('TodoApp', [])
 
 app.controller("TodoCtrl", function($scope){
-  $scope.title = "Welcome to your Todo App"
+  $scope.title = "Welcome to your Todo App";
+
+  $scope.todos = [
+    {name:"mow the lawn", complete:"incomplete"},
+    {name:"laundry", complete:"incomplete"},
+    {name:"all the naps", complete:"complete"},
+  ];
+  $scope.killTodo=function(todo){
+    var todoIndex = $scope.todos.indexOf(todo);
+    if(todoIndex>=0){
+      $scope.todos.splice(todoIndex, 1)
+    }
+
+  };
+
+    // $scope.searchText="";
+
 })
 
 
